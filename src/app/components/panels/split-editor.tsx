1d8c4133100d2dad7e752bc8f84abdd79ed93564
Ondra <ondra@noreply.com>
2024-12-05 10:25:31 +0100

    ActionBar

--- src/app/components/panels/split-editor.tsx
+++ src/app/components/panels/split-editor.tsx
@@ -3,6 +3,8 @@ import { observer } from "mobx-react-lite";
 import { useState } from "react";
 import SplitPane, { Pane } from "split-pane-react";
 import { SingleEditor } from "./single-editor";
+import { ActionBar } from "@primer/react";
+import { QuestionIcon } from "@primer/octicons-react";
 
 type props = {
     left_store: MonacoFileStore;
@@ -14,17 +16,29 @@ export const SplitEditor = observer((props: props) => {
         'auto',
     ]);
 
-    return (props.right_store)
-        ? <SplitPane
-            split="vertical"
-            sizes={sizes}
-            onChange={(sizes) => setSizes(sizes)}        >
-            <Pane minSize={20}>
-                <SingleEditor store={props.left_store} />
-            </Pane>
-            <Pane minSize={20}>
-                <SingleEditor store={props.right_store} />
-            </Pane>
-        </SplitPane>
-        : <SingleEditor store={props.left_store} />
+    return <div className="flex flex-col">
+        <div className="flex">
+            <ActionBar aria-label="ACTION BAR">
+                <ActionBar.IconButton
+                    icon={QuestionIcon}
+                    aria-label="Bold"
+                ></ActionBar.IconButton>
+            </ActionBar>
+        </div>
+        <div className="grow">
+            {(props.right_store)
+                ? <SplitPane
+                    split="vertical"
+                    sizes={sizes}
+                    onChange={(sizes) => setSizes(sizes)}        >
+                    <Pane minSize={20}>
+                        <SingleEditor store={props.left_store} />
+                    </Pane>
+                    <Pane minSize={20}>
+                        <SingleEditor store={props.right_store} />
+                    </Pane>
+                </SplitPane>
+                : <SingleEditor store={props.left_store} />}
+        </div>
+    </div>
 });
