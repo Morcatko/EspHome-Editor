f063b86fffa1486c14690629e500b68144944fff
Ondra <ondra@noreply.com>
2024-12-14 21:08:40 +0100

    fix warning

--- src/app/api/device/[device_id]/esphome/log/route.ts
+++ src/app/api/device/[device_id]/esphome/log/route.ts
@@ -2,6 +2,8 @@ import * as ws from "ws";
 import * as http from "node:http";
 import { streamToWs } from "../utils";
 
+export function GET() {}
+
 export async function SOCKET(
     client: ws.WebSocket,
     request: http.IncomingMessage,
