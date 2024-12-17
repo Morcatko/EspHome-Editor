ac0bde3884dcf8c370e39ad3ececfef75fe5ba00
Ondra <ondra@noreply.com>
2024-12-17 09:01:22 +0100

    DarkTheme

--- src/app/components/panels/diff-editor.tsx
+++ src/app/components/panels/diff-editor.tsx
@@ -1,4 +1,5 @@
 import { MonacoFileStore } from "@/app/stores/panels-store/utils/monaco-file-store";
+import { useDarkTheme } from "@/app/utils/hooks";
 import { DiffEditor as MonacoDiffEditor } from "@monaco-editor/react";
 import { observer } from "mobx-react-lite";
 
@@ -7,12 +8,16 @@ type props = {
     right_store: MonacoFileStore;
 }
 export const DiffEditor = observer((props: props) => {
+    const isDarkMode = useDarkTheme();
+
     return <MonacoDiffEditor
-            original={props.left_store.content}
-            modified={props.right_store.content}
-            originalLanguage={props.left_store.language}
-            modifiedLanguage={props.right_store.language}
-            options={{
-                readOnly: props.left_store?.readonly,
-            }} />
+        original={props.left_store.content}
+        modified={props.right_store.content}
+        originalLanguage={props.left_store.language}
+        modifiedLanguage={props.right_store.language}
+        theme={isDarkMode ? "vs-dark" : "vs-light"}
+        options={{
+            readOnly: props.left_store?.readonly,
+        }}
+    />
 });
