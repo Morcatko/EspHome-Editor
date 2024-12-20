5f8f5a7735085f29374e12c1d9f1ce081c70be67
Ondra <ondra@noreply.com>
2024-12-20 13:46:24 +0100

    Feedback link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Heading, Spinner } from "@primer/react";
 import { observer } from "mobx-react-lite";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "./utils/api-client";
+import { LinkExternalIcon } from "@primer/octicons-react";
 
 export default observer(() => {
 	const store = useStore();
@@ -28,7 +29,10 @@ export default observer(() => {
 				<Heading className="inline-block align-baseline text-slate-600 dark:text-slate-400" variant="small" >Editor for ESPHome</Heading>
 			</div>
 			<div style={{ gridArea: "2/1/2/1" }} className="pl-1 overflow-y-auto"><DevicesTreeView /></div>
-			<div style={{ gridArea: "3/1/3/1" }} className="border-t border-slate-200 text-center text-slate-400 ">{query.isSuccess && query.data?.version}</div>
+			<div style={{ gridArea: "3/1/3/1" }} className="border-t border-slate-200 text-center">
+				<div className="text-slate-400">{query.isSuccess && query.data?.version}</div>
+				<div className="text-blue-300 text-sm underline"><a href="https://github.com/Morcatko/EspHome-Editor/issues" target="_blank">feedback <LinkExternalIcon className="inline" /></a></div>
+			</div>
 			<div style={{ gridArea: "1/2/4/2" }} className="border-l border-slate-200"><PanelsContainer /></div>
 		</div>
 });
