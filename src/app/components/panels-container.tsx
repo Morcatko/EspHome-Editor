a899721c2cbd910d2c3e78b7a43b7ec457468008
Ondra <ondra@noreply.com>
2024-12-26 20:44:12 +0100

    FlexLayout test

--- src/app/components/panels-container.tsx
+++ src/app/components/panels-container.tsx
@@ -13,8 +13,9 @@ import { LocalFilePanel } from "./panels/local-file-panel";
 import { LocalDevicePanel } from "./panels/local-device-panel";
 import { ESPHomeDevicePanel } from "./panels/esphome-device-panel";
 import { DiffPanel } from "./panels/diff-panel";
+import { Actions, CLASSES, Layout, Model, TabNode } from "flexlayout-react";
 
-const PanelContent = observer(({ tabStore } : {tabStore: IPanelsStore}) => {
+const PanelContent = observer(({ tabStore }: { tabStore: IPanelsStore }) => {
     if (tabStore instanceof ESPHomeDeviceStore) {
         return <ESPHomeDevicePanel device_id={tabStore.device.id} />;
     } else if (tabStore instanceof LocalFileStore) {
@@ -36,6 +37,65 @@ const PanelContent = observer(({ tabStore } : {tabStore: IPanelsStore}) => {
     return <div>Noting selected</div>;
 });
 
+
+const factory = (node: TabNode) => {
+    var component = node.getComponent();
+
+    return <div>{component}</div>
+}
+
+var json = {
+    global: {},
+    borders: [{
+        "type": "border",
+        "location": "left",
+        enableAutoHide: true,
+        selected: 0,
+        "children": [
+            {
+                "type": "tab",
+                "name": "Devices",
+                "component": "devices",
+                "enableClose": false,
+            }
+        ]
+    },{
+        "type": "border",
+        enableAutoHide: true,
+        "location": "right"
+    }],
+    layout: {
+        type: "row",
+        weight: 100,
+        children: [
+            {
+                type: "tabset",
+                weight: 50,
+                children: [
+                    {
+                        type: "tab",
+                        name: "One",
+                        component: "button",
+                    }
+                ]
+            },
+            {
+                type: "tabset",
+                weight: 50,
+                children: [
+                    {
+                        type: "tab",
+                        name: "Two",
+                        component: "button",
+                    }
+                ]
+            }
+        ]
+    }
+};
+
+const model = Model.fromJson(json);
+
 export const PanelsContainer = observer(() => {
     const store = useStore();
 
@@ -44,7 +104,12 @@ export const PanelsContainer = observer(() => {
     tabStore?.loadIfNeeded();
 
     if (!tabStore) {
-        return null;
+        return <div className="relative h-full w-full"><Layout
+            // classNameMapper={classNameMapper}
+            model={model}
+            factory={factory}
+        />
+        </div>;
     }
 
     return <div style={{
@@ -58,5 +123,5 @@ export const PanelsContainer = observer(() => {
         </div>
         <PanelContent tabStore={tabStore} />
     </div>
-    
+
 });
