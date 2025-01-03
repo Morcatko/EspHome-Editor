48cc9fe348af590a736f8af4b572a8aa60f7f800
Ondra <ondra@noreply.com>
2025-01-03 10:02:37 +0100

    Fix Codicon

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+//Middleware to fix wrong url of codicon font (Monaco-editor)
+export function middleware(request: NextRequest) {
+    const pathName = request.nextUrl.pathname.replace('/_next/static/css/_next/static/', '/_next/static/');
+    const newUrl = new URL(pathName, request.url)
+    return NextResponse.rewrite(newUrl);
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/_next/static/css/_next/static/:path*',
+}
