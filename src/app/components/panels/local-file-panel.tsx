869880307ac407b83120eb97a9f709418c74e956
Ondra <ondra@noreply.com>
2025-01-03 13:12:12 +0100

    dockview works

--- src/app/components/panels/local-file-panel.tsx
+++ src/app/components/panels/local-file-panel.tsx
@@ -1,8 +1,5 @@
 import { useLocalFileStore } from "@/app/stores/panels-store/local-file-store";
 import { SplitEditor } from "../editors/split-editor";
-import { useEffect, useState } from "react";
-import { DockviewApi, DockviewDefaultTab, DockviewReact, DockviewReadyEvent, IDockviewPanelProps } from "dockview-react";
-import { SingleEditor } from "../editors/single-editor";
 
 // const EtaJsBanner = ({ store }: { store: LocalFileStore }) => {
 //     const [sizes, setSizes] = useState<(number | string)[]>([
@@ -39,56 +36,10 @@ import { SingleEditor } from "../editors/single-editor";
 //     }
 // }
 
-type TParams = {
-    device_id: string;
-    file_path: string;
-}
-
-const components = {
-    src: (p: IDockviewPanelProps<TParams>) => { const data = useLocalFileStore(p.params.device_id, p.params.file_path); return <SingleEditor {...data.leftEditor} />; },
-    tgt: (p: IDockviewPanelProps<TParams>) => { const data = useLocalFileStore(p.params.device_id, p.params.file_path); return <SingleEditor {...data.rightEditor!} />; },
-};
 export const LocalFilePanel = ({ device_id, file_path }: { device_id: string, file_path: string }) => {
-    //const data = useLocalFileStore(device_id, file_path);
+    const data = useLocalFileStore(device_id, file_path);
 
-    const [api, setApi] = useState<DockviewApi | null>(null);
-
-    const onReady = (event: DockviewReadyEvent) => setApi(event.api);
-
-    useEffect(() => {
-        if (!api) {
-            return;
-        }
-
-        const panel_src = api.panels.find(p => p.id === 'panel_src');
-        if (panel_src)
-            panel_src?.update({ params: { device_id, file_path } });
-        else
-            api.addPanel<TParams>({
-                id: 'panel_src',
-                component: 'src',
-                params: { device_id, file_path },
-            });
-            
-        const panel_tgt = api.panels.find(p => p.id === 'panel_tgt');
-        if (panel_tgt)
-            panel_tgt?.update({ params: { device_id, file_path } });
-        else
-            api.addPanel<TParams>({
-                id: 'panel_tgt',
-                component: 'tgt',
-                params: { device_id, file_path },
-            });
-    }, [device_id, file_path]);
-
-    return <DockviewReact
-        className='dockview-theme-light'
-        onReady={onReady}
-        components={components}
-        defaultTabComponent={(props) => <DockviewDefaultTab {...props} hideClose />}
-    />;
-    /*
-  return <SplitEditor
+    return <SplitEditor
       leftEditor={data.leftEditor} 
-      rightEditor={data.rightEditor} />;*/
+      rightEditor={data.rightEditor} />;
 };
