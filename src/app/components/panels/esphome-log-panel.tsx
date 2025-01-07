e524ca29a0e032933b9b6df60e440fa54f330204
Ondra <ondra@noreply.com>
2025-01-07 21:48:46 +0100

    Virtualized List

--- src/app/components/panels/esphome-log-panel.tsx
+++ src/app/components/panels/esphome-log-panel.tsx
@@ -1,7 +1,7 @@
 import { useEspHomeLogStore } from "@/app/stores/panels-store/esphome-log-store";
-import { HtmlViewer } from "../editors/html-viewer";
+import { LogStream } from "../editors/log-stream";
 
 export const EspHomeLogPanel = ({ device_id }: { device_id: string }) => {
     const data = useEspHomeLogStore(device_id);
-    return <HtmlViewer data={data} />;
+    return <LogStream data={data} />;
 }
