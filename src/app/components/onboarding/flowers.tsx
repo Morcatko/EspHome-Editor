c13dbf0a0ab708e538fa0f06386b8f1eecc22f1e
Ondra <ondra@noreply.com>
2025-01-12 12:50:03 +0100

    text

--- src/app/components/onboarding/flowers.tsx
+++ src/app/components/onboarding/flowers.tsx
@@ -1,12 +1,30 @@
+import { SingleEditor } from "../editors/single-editor";
+import { Heading, Section } from "./components";
+
 export const Flowers = () => {
     return <>
-             <div className="text-center">
-            <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
-                Flowers
-            </h1>
-            <p className="text-gray-600 dark:text-gray-400 mt-2">
-                We will make a sensor, that can be attached to every flower in your home
-            </p>
-        </div>
+        <Heading
+            title="Flwers"
+            subtitle="We will make an automated plant watering system for all flowers in your house.
+                Each device (a.k.a. plant) will have a moisture sensor, light sensor and a motor to water your plant.
+                Additionally each plan will have its own watering schedule." />
+
+        <Section
+            step="flower.shared"
+            title="Shared configuration">
+            We will start by making a file in .lib folder called demo-flower.eta
+            <div
+                style={{ height: "300px" }}>
+                <p>.lib/demo-flower.eta</p>
+                <SingleEditor
+                    language="yaml"
+                    value="hello" />
+            </div></Section>
+        <Section
+            step="flower.flower-1"
+            title="Flower device">
+
+            We will create a new device called flower-1 in the demo-flower.eta file.
+        </Section>
     </>;
 }
