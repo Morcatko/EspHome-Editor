1551ccff11f713b2f23ec6763e37e7e2a9c3c889
Ondra <ondra@noreply.com>
2025-01-13 16:23:07 +0100

    flower

--- src/app/components/onboarding/flowers.tsx
+++ src/app/components/onboarding/flowers.tsx
@@ -1,6 +1,59 @@
 import { SingleEditor } from "../editors/single-editor";
 import { Heading, Section } from "./components";
 
+const demo_flower_template_eta = 
+`esphome:
+  name: <%= it.name %>
+
+esp32:
+  board: m5stack-atom
+  framework:
+    type: arduino
+
+api:
+ota:
+    platform: esphome
+
+wifi:
+  ssid: !secret wifi_ssid
+  password: !secret wifi_password
+
+
+sensor:
+  - platform: adc
+    pin: 32
+    name: Soil Moisture Value
+    attenuation: 11db
+    id:  soil_moisture
+    accuracy_decimals: 0
+    unit_of_measurement: "%"
+    update_interval: 10s
+    filters:
+      - calibrate_linear:
+        - 1.795 -> 0.0
+        - 1.43 -> 100.0
+    on_value_range:
+      - below: <%= it.moisture_limit %>
+        then:
+          - switch.turn_on: relay
+      - above: <%= it.moisture_limit %>
+        then:
+          - switch.turn_off: relay
+
+switch:
+  - platform: gpio
+    pin: 26
+    id: relay
+    name: "Water pump"`;
+
+const configuration_eta =
+`<%~ include('../.lib/demo-flower-template', 
+    {
+        name: 'flower-1', 
+        moisture_limit: 35
+    })
+%>`;
+
 export const Flowers = () => {
     return <>
         <Heading
@@ -12,28 +65,29 @@ export const Flowers = () => {
             step="flower.shared"
             title="1. Shared Configuration">
             We will start by making a file in .lib folder called demo-flower-template.eta. Click on "..." menu in .lib folder and select "New file".
-
             <div
                 style={{ height: "350px" }}>
                 <p>.lib/demo-flower-template.eta</p>
                 <SingleEditor
                     language="yaml"
-                    value="hello" />
-            </div></Section>
+                    value={demo_flower_template_eta} />
+            </div>
+          </Section>
         <Section
             step="flower.flower-1"
             title="2. Flower Device">
-
             Now create a new device called flower-1.
             Use a "Create device" button in devices panel. This will automatically create a file "configuration.yaml" for you. ¨
             Rename it to configuration.eta and edit with following content.
+            <p>flower-1/configuration.eta</p>
             <div
-                style={{ height: "250px" }}>
-                <p>flower-1/configuration.eta</p>
+                style={{ height: "150px" }}>
                 <SingleEditor
+                  
                     language="yaml"
-                    value="hello" />
+                    value={configuration_eta} />
             </div>
+            Notice the `&lt;%= it.name %&gt;` and  `&lt;%= it.moisture_limit %&gt;` placeholders. These are used to inject values from the configuration.eta file.
         </Section>
 
         <Section
