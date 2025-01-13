5bb0558675dd5e71d4e97e80efed104cf8f7792f
Ondra <ondra@noreply.com>
2025-01-13 12:13:29 +0100

    better padding

--- src/app/components/onboarding/index.tsx
+++ src/app/components/onboarding/index.tsx
@@ -10,7 +10,7 @@ export const Onboarding = () => {
 
     return <div className='overflow-auto h-full'>
         {page !== "home" && <div>Breadcrumbs</div>}
-        <div className="mx-auto p-8">
+        <div className="mx-auto py-8 px-2">
             <div className="max-w-3xl mx-auto space-y-6">
                 {page === "home" && <Home onCLick={setPage} />}
                 {page === "flowers" && <Flowers />}
