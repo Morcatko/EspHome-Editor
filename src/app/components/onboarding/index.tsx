7a0974cb0b9e50333fe2043a2541ea9c14b3030a
Ondra <ondra@noreply.com>
2025-01-17 13:46:55 +0100

    Breadcrumbs

--- src/app/components/onboarding/index.tsx
+++ src/app/components/onboarding/index.tsx
@@ -1,15 +1,26 @@
 import { Home } from './home';
 import { Flowers } from './flowers';
 import { TPanel_Onboarding } from '@/app/stores/panels-store/types';
+import { Breadcrumbs, Link } from '@mui/joy';
+import { usePanelsStore } from '@/app/stores/panels-store';
+import { ChevronRightIcon } from '@primer/octicons-react';
 
 export const Onboarding = ({ panel }: { panel: TPanel_Onboarding }) => {
+    const panelsStore = usePanelsStore();
+
     const isHome = (!panel.step || (panel.step === "home"));
+    const isFlowers = (panel.step === "flowers");
     return <div className='overflow-auto h-full'>
-        {!isHome && <div>Breadcrumbs</div>}
+        {!isHome && <Breadcrumbs separator={<ChevronRightIcon />} aria-label="breadcrumbs">
+            <Link color="neutral" onClick={(e) => panelsStore.addPanel(e, { operation: "onboarding", step: "home" })} >
+                Home
+            </Link>
+            {isFlowers && <div>Flowers</div>}
+        </Breadcrumbs>}
         <div className="mx-auto py-8 px-2">
             <div className="max-w-3xl mx-auto space-y-6">
                 {isHome && <Home />}
-                {panel.step === "flowers" && <Flowers />}
+                {isFlowers && <Flowers />}
             </div>
         </div>
     </div>;
