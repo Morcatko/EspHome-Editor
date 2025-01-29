5e9021c2fc248b741a14bdcebfed3ee75183c757
Ondra <ondra@noreply.com>
2025-01-29 09:18:19 +0100

    LocalFile Improvements

--- src/app/components/panels/local-file-panel.tsx
+++ src/app/components/panels/local-file-panel.tsx
@@ -1,41 +1,6 @@
 import { useLocalFileStore } from "@/app/stores/panels-store/local-file-store";
 import { SplitEditor } from "../editors/split-editor";
-import { useState } from "react";
 
-// const EtaJsBanner = ({ store }: { store: LocalFileStore }) => {
-//     const [sizes, setSizes] = useState<(number | string)[]>([
-//         '50%',
-//         'auto',
-//     ]);
-
-//     return <SplitPane
-//         split="vertical"
-//         sizes={sizes}
-//         onChange={(sizes) => setSizes(sizes)} >
-//         <Pane minSize={20}>
-//             <Editor
-//                 language={store.test_file?.language}
-//                 options={{ minimap: { enabled: false }, lineNumbers: "off" }}
-//                 value={store.test_file?.content}
-//                 onChange={(v) => store.test_file?.changeContent(v ?? "")} />
-//         </Pane>
-//         <Pane minSize={20}>
-//             <ul className="flex-grow text-right">
-//                 <li><Link href="https://eta.js.org/docs/intro/template-syntax" target="_blank" rel="noreferrer">Template syntax</Link></li>
-//                 <li><Link href="https://eta.js.org/docs/intro/syntax-cheatsheet" target="_blank" rel="noreferrer">Syntax cheatsheat</Link></li>
-//             </ul>
-//         </Pane>
-//     </SplitPane>;
-// }
-
-// const getBanner = (store: LocalFileStore) => {
-//     switch (store.file.compiler) {
-//         /*case "etajs":
-//             return <EtaJsBanner store={store} />;*/
-//         default:
-//             return null;
-//     }
-// }
 export const LocalFilePanel = ({ device_id, file_path }: { device_id: string, file_path: string }) => {
     const data = useLocalFileStore(device_id, file_path);
 
