581bda5d0723164adf9c3caecc3eea75e214bd19
Ondra <ondra@noreply.com>
2025-02-16 21:08:47 +0100

    fix merge

--- src/app/components/editors/monaco/next-init.ts
+++ src/app/components/editors/monaco/next-init.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { loader } from "@monaco-editor/react";
+import { initEspHomeLanguage } from './esphome-language';
 
 export const useMonacoInit = () => {
     const [loaded, setLoaded] = useState(false);
@@ -7,7 +8,7 @@ export const useMonacoInit = () => {
         //Monaco editor based on https://github.com/graphql/graphiql/blob/main/examples/monaco-graphql-nextjs/next.config.js
         import('monaco-editor').then(m => {
             loader.config({ monaco: m });
-            m
+            initEspHomeLanguage();
             setLoaded(true);
         });
     }, []);
