4978509dbb3360906d1e5cbaad5b6fc3a8381da0
Ondra <ondra@noreply.com>
2025-02-16 13:18:40 +0100

    improved init

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -2,21 +2,15 @@
 import { ThemeProvider } from '@primer/react'
 import { queryClient } from "./stores";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { useMonacoInit } from "./components/editors/monaco/next-init";
 
 export function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const monacoInitialized = useMonacoInit();
-
-  return monacoInitialized
-    ? (<QueryClientProvider client={queryClient}>
-      <ThemeProvider colorMode="auto" preventSSRMismatch>
-        {children}
-      </ThemeProvider>
-    </QueryClientProvider>
-    )
-    : null;
+  return <QueryClientProvider client={queryClient}>
+    <ThemeProvider colorMode="auto" preventSSRMismatch>
+      {children}
+    </ThemeProvider>
+  </QueryClientProvider>;
 }
