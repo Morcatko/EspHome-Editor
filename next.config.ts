0ac2a3de8e3fbd46e1edba5ff650a0fee016848d
Ondra <ondra@noreply.com>
2025-02-16 13:30:21 +0100

    cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const { patchWebpackConfig } = require("next-global-css");
-
-//Monaco editor based on https://github.com/graphql/graphiql/blob/main/examples/monaco-graphql-nextjs/next.config.js
+import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -16,44 +13,21 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  webpack(config, options) {
-    console.log("config", config);
-    // this fixes some issues with loading web workers
-    config.output.publicPath = "/_next/";
-    // because next.js doesn't like node_modules that import css files - this solves the issue for monaco-editor, which relies on importing css files
-    patchWebpackConfig(config, options);
-
-    // alias the inlined, vscode forked marked implementation to the actual library
-    config.resolve.alias["../common/marked/marked.js"] = "marked";
-
-    // adding monaco-editor to your bundle is slow as is, no need to bundle it for the server in addition to the client
+  //https://github.com/Chenalejandro/next.js/blob/add-monaco-editor-example/examples/monaco-editor/next.config.mjs
+  //https://github.com/microsoft/monaco-editor/blob/main/docs/integrate-esm.md
+  webpack: (config, options) => {
     if (!options.isServer) {
       config.plugins.push(
-        // if you find yourself needing to override
-        // MonacoEnvironment.getWorkerUrl or MonacoEnvironment.getWorker,
-        // you probably just need to tweak configuration here.
         new MonacoWebpackPlugin({
           // you can add other languages here as needed
-          languages: ["yaml"],
-          filename: "static/[name].worker.js",
-          // this option is not in the plugin readme, but saves us having to override MonacoEnvironment.getWorkerUrl or similar.
-          /*customLanguages: [
-            {
-              label: "graphql",
-              worker: {
-                id: "graphql",
-                entry: require.resolve("monaco-graphql/esm/graphql.worker.js"),
-              },
-            },
-          ],*/
+          // (list of languages: https://github.com/microsoft/monaco-editor/tree/main/src/basic-languages)
+          //languages: ['javascript', 'typescript', 'php', 'python'],
+          filename: 'static/[name].worker.[contenthash].js',
         })
-      );
+      )
     }
-    //load monaco-editor provided ttf fonts
-    config.module.rules.push({ test: /\.ttf$/, type: "asset/resource" });
-
-    return config;
-  }
+    return config
+  },
 };
 
 export default nextConfig;
