f9798d4605c3b831d6eb72ef67e16f7995970fc2
Ondra <ondra@noreply.com>
2025-03-04 09:21:33 +0100

    flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { Anchor, Loader } from "@mantine/core";
 import Image from "next/image";
 import { DevicesTree } from "./components/devices-tree";
@@ -18,12 +18,10 @@ const devicesPanel: TPanel = {
 
 const Header = () => {
 	const panelsStore = usePanelsStore();
-	return <div style={{ gridArea: "1/1/1/1", lineHeight: '56px' }} className="border-b border-slate-200 dark:border-slate-800 text-center" >
-		<a href="#" onClick={() => panelsStore.addPanel(devicesPanel)}>
-			<Image className="inline mr-2 align-middle" src={logo} alt="ESPHome Editor" width="32" height="32" />
-			<h4 className="inline-block align-baseline text-slate-600 dark:text-slate-400 m-0 font-semibold" >Editor for ESPHome</h4>
-		</a>
-	</div>
+	return <a href="#" onClick={() => panelsStore.addPanel(devicesPanel)}>
+		<Image className="inline mr-2 align-middle" src={logo} alt="ESPHome Editor" width="32" height="32" />
+		<h4 className="inline-block align-baseline text-slate-600 dark:text-slate-400 m-0 font-semibold" >Editor for ESPHome</h4>
+	</a>
 }
 
 const Page = () => {
@@ -39,17 +37,27 @@ const Page = () => {
 			<Loader className="content-center" />
 		</div>
 		: <Suspense>
-			{devicePanelExists
-				? <div className="h-screen border-l border-slate-200 dark:border-slate-800 relative"><PanelsContainer /></div>
-				: <div style={{ gridTemplateColumns: "18rem 1fr", gridTemplateRows: "56px 1fr auto", gridGap: "1px" }} className="h-screen w-screen grid" >
-					<Header />
-					<div style={{ gridArea: "2/1/2/1" }} className="pl-1 overflow-y-auto"><DevicesTree /></div>
-					<div style={{ gridArea: "3/1/3/1" }} className="border-t border-slate-200 dark:border-slate-800 text-center p-6">
-						<Anchor href="#" onClick={() => openAboutDialog()}>{statusStore.query.isSuccess && statusStore.query.data?.version}</Anchor>
+			<div className="h-screen w-screen flex" >
+				{!devicePanelExists
+					? <div className="flex-none w-2xs flex flex-col">
+						<div className="flex-none border-b border-slate-200 dark:border-slate-800 text-center leading-[56px]" >
+							<Header />
+						</div>
+						<div className="flex-grow pl-1 overflow-y-auto">
+							<DevicesTree />
+						</div>
+						<div className="flex-none border-t border-slate-200 dark:border-slate-800 text-center p-6 flex">
+							<div className="w-14 flex-none">
+								Col
+							</div>
+							<Anchor className="flex-grow" href="#" onClick={() => openAboutDialog()}>{statusStore.query.isSuccess && statusStore.query.data?.version}</Anchor>
+						</div>
 					</div>
-					<div style={{ gridArea: "1/2/4/2" }} className="border-l border-slate-200 dark:border-slate-800 relative"><PanelsContainer /></div>
+					: null}
+				<div className="flex-grow border-l border-slate-200 dark:border-slate-800 relative">
+					<PanelsContainer />
 				</div>
-			}
+			</div>
 		</Suspense>
 };
 export default Page;
