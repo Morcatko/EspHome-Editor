2f25702b3e2895621613c1c92bc1fa69e0ededed
Ondra <ondra@noreply.com>
2025-04-04 14:16:48 +0200

    Loading Wrapper

--- src/app/components/editors/diff-editor.tsx
+++ src/app/components/editors/diff-editor.tsx
@@ -1,6 +1,7 @@
 import { TEditorFileProps } from "@/app/stores/panels-store/types";
 import { DiffEditor as MonacoDiffEditor } from "@monaco-editor/react";
 import { useMonacoTheme } from "./monaco/useMonacoTheme";
+import { ContentLoadingWrapper } from "./loading-wrappers";
 
 type TProps = {
     leftEditor: TEditorFileProps;
@@ -10,14 +11,8 @@ type TProps = {
 export const DiffEditor = (props: TProps) => {
     const theme = useMonacoTheme();
 
-    if (props.leftEditor.value.pending || props.rightEditor.value.pending) {
-        return <div>Loading...</div>;
-    }
-    else if (props.leftEditor.value.error || props.rightEditor.value.error) {
-        return <div>{props.leftEditor.value.error || props.rightEditor.value.error || "Something went wrong"}</div>;
-    }
-    else {
-        return <MonacoDiffEditor
+    return <ContentLoadingWrapper value={props.leftEditor.value} >
+        <MonacoDiffEditor
             original={props.leftEditor.value.content}
             modified={props.rightEditor.value.content}
             originalLanguage={props.leftEditor.language}
@@ -27,5 +22,5 @@ export const DiffEditor = (props: TProps) => {
                 readOnly: !!props.leftEditor.onValueChange,
             }}
         />
-    }
+    </ContentLoadingWrapper>;
 };
--- src/app/components/editors/html-preview.tsx
+++ src/app/components/editors/html-preview.tsx
@@ -1,18 +1,13 @@
 import { TEditorFileProps } from "@/app/stores/panels-store/types";
+import { ContentLoadingWrapper } from "./loading-wrappers";
 
 type THtmlPreviewProps = TEditorFileProps;
 
 export const HtmlPreview = (props: THtmlPreviewProps) => {
-    if (props.value.pending) {
-        return <div>Loading...</div>;
-    }
-    else if (props.value.error) {
-        return <div>{props.value.content || "Something went wrong"}</div>;
-    }
-    else {
-        return <div
-                className="px-8"
-                dangerouslySetInnerHTML={{__html: props.value.content}}
+    return <ContentLoadingWrapper value={props.value} >
+        <div
+            className="px-8"
+            dangerouslySetInnerHTML={{ __html: props.value.content }}
         />
-    }
+    </ContentLoadingWrapper>;
 };
