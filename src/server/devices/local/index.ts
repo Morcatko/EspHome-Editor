5f0a0dbf28f893759faeb23806d95c2bcbe9e048
Ondra <ondra@noreply.com>
2025-04-26 10:27:30 +0200

    cleanup

--- src/server/devices/local/index.ts
+++ src/server/devices/local/index.ts
@@ -3,7 +3,7 @@ import fs from "node:fs/promises";
 import type { TDevice, TLocalDirectory, TLocalFile, TLocalFileOrDirectory } from "../types";
 import { c } from "../../config";
 import { directoryExists, fileExists, listDirEntries } from "../../utils/fs-utils";
-import { compileFile as _compileFile, getFileInfo } from "./template-processors";
+import { compileFile, getFileInfo } from "./template-processors";
 import { log } from "@/shared/log";
 import { mergeEspHomeYamlFiles } from "./template-processors/yaml-merger";
 import { patchEspHomeYaml } from "./template-processors/yaml-patcher";
@@ -29,7 +29,7 @@ const scanDirectory = async (device_id: string, fullPath: string, parentPath: st
                         id: e.name,
                         name: e.name,
                         path: path,
-                        enabled:  enabled,
+                        enabled: enabled,
                         type: "directory",
                         files: await scanDirectory(device_id, `${fullPath}/${e.name}`, path, enabled),
                     };
@@ -53,157 +53,170 @@ const scanDirectory = async (device_id: string, fullPath: string, parentPath: st
         .sort((a, b) => a.type.localeCompare(b.type));
 };
 
-export namespace local {
-    export const getDevices = async (): Promise<TDevice[]> => {
-        log.debug("Getting Local devices");
-        const deviceDirectories = await listDirEntries(
-            c.devicesDir,
-            (d) => d.isDirectory(),
-        );
+const getDevices = async (): Promise<TDevice[]> => {
+    log.debug("Getting Local devices");
+    const deviceDirectories = await listDirEntries(
+        c.devicesDir,
+        (d) => d.isDirectory(),
+    );
 
-        const resAsync = deviceDirectories
-            .map(async (d) => {
-                return <TDevice>{
-                    id: d.name,
-                    path: "",
-                    name: d.name,
-                    type: "device",
-                    files: await scanDirectory(d.name, `${c.devicesDir}/${d.name}`, null, true),
-                } as TDevice;
-            });
+    const resAsync = deviceDirectories
+        .map(async (d) => {
+            return <TDevice>{
+                id: d.name,
+                path: "",
+                name: d.name,
+                type: "device",
+                files: await scanDirectory(d.name, `${c.devicesDir}/${d.name}`, null, true),
+            } as TDevice;
+        });
 
-        return awaitArray(resAsync);
+    return awaitArray(resAsync);
+}
+
+const createDirectory = async (device_id: string, path: string) => {
+    await ensureDeviceDirExists(device_id);
+    const fullPath = getDevicePath(device_id, path);
+    await fs.mkdir(fullPath);
+}
+
+const createFile = async (device_id: string, path: string) => {
+    await saveFileContent(device_id, path, "");
+}
+
+const getFileContent = async (device_id: string, file_path: string) => {
+    const path = getDevicePath(device_id, file_path);
+    return await fs.readFile(path, "utf-8");
+}
+
+const tryGetFileContent = async (device_id: string, file_path: string) => {
+    const path = getDevicePath(device_id, file_path);
+    return await fileExists(path) ? await fs.readFile(path, "utf-8") : null;
+}
+
+const saveFileContent = async (device_id: string, file_path: string, content: string) => {
+    await ensureDeviceDirExists(device_id);
+    const path = getDevicePath(device_id, file_path);
+    await fs.writeFile(path, content, "utf-8");
+}
+
+
+const createDevice = async (device_id: string) =>
+    await fs.mkdir(getDeviceDir(device_id));
+
+const toggleEnabled = async (device_id: string, path: string) => {
+    await ManifestUtils.togglePathEnabled(device_id, path);
+}
+
+const renameFile = async (device_id: string, path: string, newName: string) => {
+    const oldPath = getDevicePath(device_id, path);
+    const parentDir = dirname(oldPath);
+    const newPath = join(parentDir, fixPath(newName));
+    log.debug(`Renaming file '${oldPath}' to '${newName}'`);
+    await fs.rename(oldPath, newPath);
+    await ManifestUtils.renameFile(device_id, path, newName);
+};
+
+const deletePath = async (device_id: string, path: string) => {
+    const fullPath = getDevicePath(device_id, path);
+    log.debug(`Deleting file '${fullPath}'`);
+    const stats = await fs.stat(fullPath);
+    if (stats.isDirectory())
+        await fs.rmdir(fullPath)
+    else {
+        await fs.unlink(fullPath);
+        await ManifestUtils.deleteFile(device_id, path);
     }
+}
 
-    export const createDirectory = async (device_id: string, path: string) => {
-        await ensureDeviceDirExists(device_id);
-        const fullPath = getDevicePath(device_id, path);
-        await fs.mkdir(fullPath);
+const deleteDevice = async (device_id: string) => {
+    const fullPath = getDevicePath(device_id, "");
+    if (await directoryExists(fullPath)) {
+        log.debug(`Deleting device '${fullPath}'`);
+        await fs.rm(fullPath, { recursive: true });
     }
+}
 
-    export const createFile = async (device_id: string, path: string) => {
-        await saveFileContent(device_id, path, "");
-    }
+const flattenTree = (tree: TLocalFileOrDirectory[]): TLocalFileOrDirectory[] => {
+    const res: TLocalFileOrDirectory[] = [];
 
-    export const getFileContent = async (device_id: string, file_path: string) => {
-        const path = getDevicePath(device_id, file_path);
-        return await fs.readFile(path, "utf-8");
-    }
-
-    export const tryGetFileContent = async (device_id: string, file_path: string) => {
-        const path = getDevicePath(device_id, file_path);
-        return await fileExists(path) ? await fs.readFile(path, "utf-8") : null;
-    }
-
-    export const saveFileContent = async (device_id: string, file_path: string, content: string) => {
-        await ensureDeviceDirExists(device_id);
-        const path = getDevicePath(device_id, file_path);
-        await fs.writeFile(path, content, "utf-8");
-    }
-
-    export const compileFile = _compileFile;
-
-    export const createDevice = async (device_id: string) =>
-        await fs.mkdir(getDeviceDir(device_id));
-
-    export const toggleEnabled = async (device_id: string, path: string) => {
-        await ManifestUtils.togglePathEnabled(device_id, path);
-    }
-
-    export const renameFile = async (device_id: string, path: string, newName: string) => {
-        const oldPath = getDevicePath(device_id, path);
-        const parentDir = dirname(oldPath);
-        const newPath = join(parentDir, fixPath(newName));
-        log.debug(`Renaming file '${oldPath}' to '${newName}'`);
-        await fs.rename(oldPath, newPath);
-        await ManifestUtils.renameFile(device_id, path, newName);
-    };
-
-    export const deletePath = async (device_id: string, path: string) => {
-        const fullPath = getDevicePath(device_id, path);
-        log.debug(`Deleting file '${fullPath}'`);
-        const stats = await fs.stat(fullPath);
-        if (stats.isDirectory())
-            await fs.rmdir(fullPath)
-        else {
-            await fs.unlink(fullPath);
-            await ManifestUtils.deleteFile(device_id, path);
+    for (const entry of tree.filter(e => e.enabled)) {
+        res.push(entry);
+        if (entry.type === "directory") {
+            const subEntries = flattenTree(entry.files ?? []);
+            res.push(...subEntries);
         }
     }
 
-    export const deleteDevice = async (device_id: string) => {
-        const fullPath = getDevicePath(device_id, "");
-        if (await directoryExists(fullPath)) {
-            log.debug(`Deleting device '${fullPath}'`);
-            await fs.rm(fullPath, { recursive: true });
+    return res;
+}
+
+
+const compileDevice = async (device_id: string) => {
+    log.debug("Compiling device", device_id);
+    const deviceDirectory = getDeviceDir(device_id);
+
+    const outputYamls: string[] = [];
+    const outputPatches: string[] = [];
+
+    const file_tree = await scanDirectory(device_id, deviceDirectory, null, true);
+    const flat_list = flattenTree(file_tree);
+
+    const fileEntries = flat_list.filter(e => e.type === "file");//  await listDirEntries(deviceDirectory, (e) => e.isFile());
+    const inputFiles = fileEntries
+        .map(f => ({
+            path: f.path,
+            enabled: f.enabled,
+            info: getFileInfo(f.name)
+        }));
+
+    for (const file of inputFiles.filter(i => i.info.type === "basic")) {
+        if (!file.enabled) {
+            log.debug("Skipping disabled file", file.path);
+            continue;
         }
+        const filePath = getDevicePath(device_id, file.path)
+        log.debug("Compiling Configuration", filePath);
+        const output = await compileFile(device_id, file.path, false);
+        log.success("Compiled Configuration", filePath);
+        outputYamls.push(output);
     }
 
-    const flattenTree = (tree: TLocalFileOrDirectory[]): TLocalFileOrDirectory[] => {
-        const res: TLocalFileOrDirectory[] = [];
+    log.debug("Merging compiled configurations", device_id);
+    const mergedYaml = mergeEspHomeYamlFiles(outputYamls);
+    log.success("Merged compiled configurations", device_id);
 
-        for (const entry of tree.filter(e => e.enabled)) {
-            res.push(entry);
-            if (entry.type === "directory") {
-                const subEntries = flattenTree(entry.files ?? []);
-                res.push(...subEntries);
-            }
+    for (const file of inputFiles.filter(i => i.info.type === "patch")) {
+        if (!file.enabled) {
+            log.debug("Skipping disabled file", file.path);
+            continue;
         }
-
-        return res;
+        const filePath = getDevicePath(device_id, file.path)
+        log.debug("Compiling patch", filePath);
+        const output = await compileFile(device_id, file.path, false);
+        log.success("Compiled patch", filePath);
+        outputPatches.push(output);
     }
 
+    log.debug("Patching configuration", device_id);
+    const patchedYaml = patchEspHomeYaml(mergedYaml, outputPatches);
+    log.success("Patched configuration", device_id);
 
-    export const compileDevice = async (device_id: string) => {
-        log.debug("Compiling device", device_id);
-        const deviceDirectory = getDeviceDir(device_id);
+    return patchedYaml.toString();
+}
 
-        const outputYamls: string[] = [];
-        const outputPatches: string[] = [];
-
-        const file_tree = await scanDirectory(device_id, deviceDirectory, null, true);
-        const flat_list = flattenTree(file_tree);
-
-        const fileEntries = flat_list.filter(e => e.type === "file");//  await listDirEntries(deviceDirectory, (e) => e.isFile());
-        const inputFiles = fileEntries
-            .map(f => ({
-                path: f.path,
-                enabled: f.enabled,
-                info: getFileInfo(f.name)
-            }));
-
-        for (const file of inputFiles.filter(i => i.info.type === "basic")) {
-            if (!file.enabled) {
-                log.debug("Skipping disabled file", file.path);
-                continue;
-            }
-            const filePath = getDevicePath(device_id, file.path)
-            log.debug("Compiling Configuration", filePath);
-            const output = await compileFile(device_id, file.path, false);
-            log.success("Compiled Configuration", filePath);
-            outputYamls.push(output);
-        }
-
-        log.debug("Merging compiled configurations", device_id);
-        const mergedYaml = mergeEspHomeYamlFiles(outputYamls);
-        log.success("Merged compiled configurations", device_id);
-
-        for (const file of inputFiles.filter(i => i.info.type === "patch")) {
-            if (!file.enabled) {
-                log.debug("Skipping disabled file", file.path);
-                continue;
-            }
-            const filePath = getDevicePath(device_id, file.path)
-            log.debug("Compiling patch", filePath);
-            const output = await compileFile(device_id, file.path, false);
-            log.success("Compiled patch", filePath);
-            outputPatches.push(output);
-        }
-
-        log.debug("Patching configuration", device_id);
-        const patchedYaml = patchEspHomeYaml(mergedYaml, outputPatches);
-        log.success("Patched configuration", device_id);
-
-        return patchedYaml.toString();
-    }
+export const local = {
+    getDevices,
+    createDirectory,
+    createFile,
+    getFileContent,
+    tryGetFileContent,
+    saveFileContent,
+    compileFile,
+    createDevice,
+    toggleEnabled,
+    renameFile,
+    deletePath,
+    deleteDevice,
+    compileDevice
 }
