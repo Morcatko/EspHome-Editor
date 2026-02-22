fc67e8bc5f7140944bcdc618b72c70302ece2c02
Ondra <ondra@noreply.com>
2026-02-22 12:59:14 +0100

    revert

--- src/app/api/device/[device_id]/esphome/install/route.ts
+++ src/app/api/device/[device_id]/esphome/install/route.ts
@@ -2,7 +2,6 @@ import * as ws from "ws";
 import * as http from "node:http";
 import { streamToWs } from "../utils";
 import { TDeviceId, TParams } from "@/app/api/api-types";
-import { espHome } from "@/server/devices/esphome";
 
 export function GET() {}
 
@@ -12,5 +11,5 @@ export async function SOCKET(
     server: ws.WebSocketServer,
     params: TParams<TDeviceId>
 ) {
-    await streamToWs(params, client, espHome.streamInstall);
+    await streamToWs(params, client, "run", { port: "OTA" });
 }
