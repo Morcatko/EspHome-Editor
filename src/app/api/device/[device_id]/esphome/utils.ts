ffd404204ae8ad7c70258166ea29b516789caded
Ondra <ondra@noreply.com>
2026-02-22 12:59:49 +0100

    revert

--- src/app/api/device/[device_id]/esphome/utils.ts
+++ src/app/api/device/[device_id]/esphome/utils.ts
@@ -1,5 +1,5 @@
-import * as ws from "ws";
-import { type TStreamEvents } from "@/server/devices/esphome";
+import { WebSocket } from "ws";
+import { espHome } from "@/server/devices/esphome";
 import { TDeviceId, TParams } from "@/app/api/api-types";
 import { log } from "@/shared/log";
 
@@ -10,9 +10,9 @@ export type TWsMessage = {
 
 export async function streamToWs(
   { params }: TParams<TDeviceId>,
-  client: ws.WebSocket,
-  streamFunc: (device_id: string, streamEvents: TStreamEvents) => Promise<WebSocket>,
-  
+  client: WebSocket,
+  path: string,
+  spawnParams: Record<string, any> | null = null
 ) {
   const { device_id } = await params;
 
@@ -21,11 +21,15 @@ export async function streamToWs(
   }
 
   return new Promise(async (res, rej) => {
-    const socket = await streamFunc(device_id!, {
-        onEvent: (e) => send("message", e.data),
-        onClose: (c) => res(c),
-        onError: (e) => rej(e)
-      });
+    const socket = await espHome
+      .stream(
+        device_id!,
+        path,
+        spawnParams,
+        (e) => send("message", e.data),
+        (c) => res(c),
+        (e) => rej(e)
+      );
     client.on("close", () => {
       log.debug("Client closed connection");
       socket.close();
