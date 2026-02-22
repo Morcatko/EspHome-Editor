5f4e3e475b125c936c1eea6aac83781317447fbf
Ondra <ondra@noreply.com>
2026-02-22 13:12:55 +0100

    delete

--- src/app/stores/panels-store/esphome-compile-store.ts
+++
@@ -1,8 +0,0 @@
-import { useDevicesStore2 } from "../devices-store";
-import { useStreamingStore } from "./utils/streaming-store";
-
-export const useEspHomeCompileStore = (device_id: string, lastClick: string) => {
-    const devicesStore =useDevicesStore2();
-    const device = devicesStore.findDevice(device_id);
-    useStreamingStore(device!.compileStreamUrl, lastClick);
-}
