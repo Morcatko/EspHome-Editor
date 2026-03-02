a87c97cd00ff7226bd60763eb130140fa1c99743
Ondra <ondra@noreply.com>
2026-03-02 12:32:42 +0100

    Devices Panel - compilation status

--- src/app/components/panels/devices/devices-panel-store.ts
+++ src/app/components/panels/devices/devices-panel-store.ts
@@ -1,10 +1,7 @@
 import { useDevicesQuery } from "@/app/stores/devices-store"
-import { api } from "@/app/utils/api-client";
 
 export const useDevicesPanelStore = () => {
     return {
-        devices_query: useDevicesQuery(),
-        refreshCompilationInfo: async (device_id: string) => api.esphome_refreshCompilationInfo(device_id),
-        refreshDeviceInfo: async (device_id: string) => api.esphome_refreshDeviceInfo(device_id),
+        devices_query: useDevicesQuery()
     };
 }
