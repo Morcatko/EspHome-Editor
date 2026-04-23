29a8b0e8b6a3b4d94119889f799491ad5812828e
Ondra <ondra@noreply.com>
2026-04-23 14:24:48 +0200

    Icons struct

--- src/app/components/devices-tree/device-toolbar.tsx
+++ src/app/components/devices-tree/device-toolbar.tsx
@@ -4,12 +4,14 @@ import { color_esphome, color_gray, color_local } from "@/app/utils/const";
 import { useDarkTheme } from "@/app/utils/hooks";
 import { TDevice } from "@/server/devices/types";
 import { ActionIcon } from "@mantine/core";
-import { BeakerIcon, CodeIcon, DownloadIcon, GitCompareIcon, LogIcon, UploadIcon } from "@primer/octicons-react";
+import { CodeIcon, DownloadIcon, GitCompareIcon, LogIcon } from "@primer/octicons-react";
 import { ToolbarItem, TToolbarButtonProps } from "../toolbar";
 import { TPanel_DeviceOperation } from "@/app/components/panels/_utils/types";
+import { Icons } from "@/app/utils/icons";
 
 type TDeviceToolbarItemProps =
-    Pick<TToolbarButtonProps, "className"> & {
+    Pick<TToolbarButtonProps, "className"> & 
+    Pick<TToolbarButtonProps, "size"> & {
         device: TDevice;
         panelTarget?: PanelTarget;
         icon?: TToolbarButtonProps["icon"];
@@ -64,11 +66,11 @@ export const DeviceToolbarItem = {
                 ? (isDarkMode ? "lightgrey" : color_gray)
                 : (isDarkMode ? color_gray : "lightgrey")} />;
     },
-    ESPHomeUpload: (p: TDeviceToolbarItemProps) => { const d = useDarkTheme(); return <DTB_Device tooltip={"Upload local to ESPHome"} icon={<UploadIcon />} onClick={(ds) => ds.espHome_upload(p.device)} color={d ? "lightgrey" : color_gray} {...p} />; },
-    ESPHomeCreate: (p: TDeviceToolbarItemProps) => { const d = useDarkTheme(); return <DTB_Device tooltip={"Create device in ESPHome"} icon={<UploadIcon />} onClick={(ds) => ds.espHome_upload(p.device)} color={d ? "lightgrey" : color_gray} {...p} />; },
+    ESPHomeUpload: (p: TDeviceToolbarItemProps) => { const d = useDarkTheme(); return <DTB_Device tooltip={"Upload local to ESPHome"} icon={Icons.UploadToESPHome} onClick={(ds) => ds.espHome_upload(p.device)} color={d ? "lightgrey" : color_gray} {...p} />; },
+    ESPHomeCreate: (p: TDeviceToolbarItemProps) => { const d = useDarkTheme(); return <DTB_Device tooltip={"Create device in ESPHome"} icon={Icons.UploadToESPHome} onClick={(ds) => ds.espHome_upload(p.device)} color={d ? "lightgrey" : color_gray} {...p} />; },
     ESPHomeShow: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Show ESPHome configuration" icon={<CodeIcon />} operation="esphome_device" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"}{...p} />; },
-    ESPHomeCompile: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Compile ESPHome configuration" icon={<BeakerIcon />} operation="esphome_compile" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"}{...p} />; },
-    ESPHomeInstall: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Install ESPHome configuration to device" icon={<UploadIcon />} operation="esphome_install" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"}{...p} />; },
+    ESPHomeCompile: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Compile ESPHome configuration" icon={Icons.Compile} operation="esphome_compile" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"}{...p} />; },
+    ESPHomeInstall: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Install ESPHome configuration to device" icon={Icons.Install} operation="esphome_install" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"}{...p} />; },
     ESPHomeLog: (p: TDeviceToolbarItemProps) => { const hasEspHomeConfig = !!p.device.esphome_config; return <DTB_Panel tooltip="Show ESPHome device log" icon={<LogIcon />} operation="esphome_log" disabled={!hasEspHomeConfig} color={hasEspHomeConfig ? color_esphome : "lightgrey"} {...p} />; },
 };
 
---
+++ src/app/utils/icons.tsx
@@ -0,0 +1,7 @@
+import { BeakerIcon, UploadIcon } from "@primer/octicons-react";
+
+export const Icons = {
+    UploadToESPHome: <UploadIcon />,
+    Compile: <BeakerIcon />,
+    Install: <UploadIcon />
+}
